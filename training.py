2defa1a7ffe2fcb4e29ca837a27a49f4d3ff97f2
LYC88888 <165794753+LYC88888@users.noreply.github.com>
2024-05-07 01:48:54 +0200

    Update training.py
    Second submission

--- training.py
+++ training.py
@@ -4,8 +4,8 @@ from imblearn.over_sampling import SMOTE
 import xgboost as xgb
 from joblib import dump
 from sklearn.metrics import f1_score
-from sklearn.metrics import accuracy_score, confusion_matrix
-from sklearn.metrics import classification_report
+from sklearn.metrics import accuracy_score, precision_recall_curve, f1_score
+
 
 # loading data (predictors)
 train = pd.read_csv("PreFer_train_data.csv", low_memory = False) 
@@ -29,31 +29,44 @@ def train_save_model(cleaned_df, outcome_df):
     y = model_df['new_child']
 
     # Split the data into training (90%) and validation sets (10%)
-    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.1, random_state=42)
-
-    # Apply SMOTE to address class imbalance by oversampling the minority class in the training data
-    smote = SMOTE(random_state=42)
-    X_train_SMOTE, y_train_SMOTE = smote.fit_resample(X_train, y_train)
-
-    # Initialize the XGBoost classifier with specific hyperparameters
-    model = xgb.XGBClassifier(objective='binary:logistic', learning_rate=0.1, n_estimators=90, use_label_encoder=False, eval_metric='logloss')
+    X_train, X_val, y_train, y_val = train_test_split(X, y, test_size=0.1, random_state=42, stratify=y)
+    
+    # With specific hyperparameters
+    model = xgb.XGBClassifier(objective='binary:logistic', learning_rate=0.1, n_estimators=50, use_label_encoder=False, eval_metric='logloss')
 
     # Train the model on the oversampled training data
-    model.fit(X_train_SMOTE, y_train_SMOTE)
+    model.fit(X_train, y_train)
+    
+    # Predict probabilities
+    y_probs_XGB_threshold = model.predict_proba(X_val)[:, 1]
+
+    # Calculate precision, recall, and thresholds
+    precision, recall, thresholds = precision_recall_curve(y_val, y_probs_XGB_threshold)
+
+    # Calculate F1 scores for each threshold
+    f1_scores = 2 * (precision * recall) / (precision + recall)
+
+    # Locate the index of the largest F1 score
+    ix = np.argmax(f1_scores)
+    # print('Best Threshold=%f, F1-Score=%.3f' % (thresholds[ix], f1_scores[ix]))
+    
+    # Using threshold to convert probabilities to binary class predictions
+    y_pred_XGB_threshold = (y_probs_XGB_threshold >= thresholds[ix]).astype(int)
+    
+    # print('Accuracy:', accuracy_score(y_val, y_pred_XGB_threshold))
+    # print('XGBoost without SMOTE + Threshold:')
+    # print(classification_report(y_val, y_pred_XGB_threshold))
     
     # Save the trained model to a file for later use
     joblib.dump(model, "model.joblib")
 
-
-
+# REAL DATA:
 import os
 # print(os.getcwd())
 # os.chdir(path to your local repository) #<---- provide the path here
 
-# preprocessing the data
-train_cleaned = clean_df(train, background_df)
+# preprocessing the function
+train_cleaned = clean_df(df, background_df)
 
 # training and saving the model
 train_save_model(train_cleaned, outcome)
-
-
